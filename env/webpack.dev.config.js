1d07d9de1ea0e53861bb3c943bb0e149d2e40384
Air <air@airs-air.kyiv.epam.com>
2016-04-09 16:44:23 +0300

    feature (build) split build to common config

--- env/webpack.dev.config.js
+++ env/webpack.dev.config.js
@@ -1,54 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
-const envConfig = require('./env.config');
-
-module.exports = {
-    debug: true,
-
+module.exports = webpackMerge(commonConfig, {
     devtool: 'cheap-source-map',
-
-    resolve: {
-        root: [envConfig.src.dir],
-        extensions: ['', '.ts', '.js']
-    },
-
-    entry: {
-        app: envConfig.src.entry
-    },
-
-    output: {
-        path: envConfig.dist.dir,
-
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[name].map'
-    },
-
-    module: {
-        preLoaders: [
-            {
-                test: /\.js/,
-                loader: 'source-map'
-            },
-            {
-                test: /\.(js|ts)/,
-                loader: 'eslint',
-                exclude: [/node_modules/]
-            }
-        ],
-        loaders: [
-            {
-                test: /\.(ts|js)$/,
-                loader: 'babel',
-                exclude: [/node_modules/]
-            },
-            {
-                test: /\.html$/,
-                loader: 'raw'
-            }
-        ]
-    },
-
     devServer: {
         port: 8000,
         host: 'localhost',
@@ -57,10 +11,5 @@ module.exports = {
             aggregateTimeout: 300,
             poll: 1000
         }
-    },
-
-    plugins: [
-        new CleanWebpackPlugin([envConfig.dist.dir], {root: envConfig.root.dir}),
-        new HtmlWebpackPlugin({template: envConfig.src.indexHtml})
-    ]
-};
+    }
+});
