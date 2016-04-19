77c539bd33801764aa22dc0ba4bcc73f3e55f393
Dmitry Golysh <disist357@gmail.com>
2016-04-19 19:45:53 +0300

    SH-10 test (build) fix es lint problems

--- src/app/example.rest.js
+++ src/app/example.rest.js
@@ -10,7 +10,7 @@ export default () => {
             source: 'client'
         };
         socket.emit('example/rest', data, resData => {
-            console.log(resData);
+            console.log(resData); // eslint-disable-line
         });
     });
 };
