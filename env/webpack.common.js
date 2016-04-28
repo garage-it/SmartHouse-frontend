7327e3ece1658eee377e9685f5eb9685fc6035d8
Dmitry Golysh <disist357@gmail.com>
2016-04-28 17:04:22 +0300

    [TG-36] [feature] Add materialize css and scss loader

--- env/webpack.common.js
+++ env/webpack.common.js
@@ -1,31 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const envConfig = require('./env.config');
 
 const jsRegexp = /\.js$/;
 
 module.exports = {
     debug: true,
-
     devtool: 'cheap-source-map',
-
     resolve: {
         root: [envConfig.src.dir],
         extensions: ['', '.ts', '.js']
     },
-
     entry: {
         app: envConfig.src.entry
     },
-
     output: {
         path: envConfig.dist.dir,
-
         filename: '[name].bundle.js',
         sourceMapFilename: '[name].map'
     },
-
     module: {
         preLoaders: [
             {
@@ -49,14 +43,29 @@ module.exports = {
                 loader: 'raw'
             },
             {
-                test: /\.css$/,
-                loader: 'raw'
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(scss|css)$/,
+                include: [/node_modules/],
+                loader: ExtractTextPlugin.extract('style', 'css')
+            },
+            {
+                test: /\.(scss|css)$/,
+                exclude: [/node_modules/],
+                loaders: ['raw', 'sass']
             }
         ]
     },
-
     plugins: [
-        new CleanWebpackPlugin([envConfig.dist.dir], {root: envConfig.root.dir}),
-        new HtmlWebpackPlugin({template: envConfig.src.indexHtml})
+        new CleanWebpackPlugin(
+            [envConfig.dist.dir],
+            {root: envConfig.root.dir}
+        ),
+        new HtmlWebpackPlugin({
+            template: envConfig.src.indexHtml
+        }),
+        new ExtractTextPlugin('main.css')
     ]
 };
