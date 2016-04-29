92627aab95df5b945e5579b73875279287e14ddf
Constantine Marinsky <Constantine.Marinsky@gmail.com>
2016-04-29 17:48:23 +0300

    [TG-35] [ref]

--- env/protractor.conf.js
+++ env/protractor.conf.js
@@ -1,20 +1,20 @@
 // An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+    // The address of a running selenium server.
+    seleniumAddress: 'http://localhost:4444/wd/hub',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'firefox'
-  },
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'firefox'
+    },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: '../e2e/*spec.js',
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: '../e2e/*spec.js',
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
 };
