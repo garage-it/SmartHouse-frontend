cd24193061d39816ddf91908bcee07e4ede6e5d8
ConstantineMarinsky <constantine.marinsky@gmail.com>
2016-05-21 18:13:51 +0300

    [TG-35] [fix] adding baseUrl parameter to test

--- e2e/dashboard_title_spec.js
+++ e2e/dashboard_title_spec.js
@@ -1,6 +1,6 @@
 describe('SmartHouse homepage', () => {
     it('should check page title', () => {
-        browser.get('https://garage-it-smart-house.herokuapp.com/');
+        browser.get(browser.baseUrl);
 		expect(element.all("//h1[text()='Smart House']"));
     });
 });
