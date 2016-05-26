37dff079357595e7d3b10596515fdc253ce71814
Denys Kondakov <Denys_Kondakov@epam.com>
2016-05-26 11:44:30 +0300

    Refactored locators.

--- e2e/pages/main.page.js
+++ e2e/pages/main.page.js
@@ -1,9 +1,9 @@
 var MainPage = function () {
     
-    this.dashboardLink = $('a[href="#/dashboard"]');
-    this.deviceListLink = $('a[href="#/device-list"]');
-    this.scenarioListLink = $('a[href="#/scenarios"]');
-    this.brandLogo = $('a.brand-logo');
+    this.dashboardLink = $('a[href*="dashboard"]');
+    this.deviceListLink = $('a[href*="device-list"]');
+    this.scenarioListLink = $('a[href*="scenarios"]');
+    this.brandLogo = $('a[href*="home"]');
 
     this.get = function () {
         browser.get(browser.baseUrl);
