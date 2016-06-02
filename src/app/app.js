995ab9c262a8baa772e17415f4d979180edb814e
Dmytro Tolok <Dmytro_Tolok@epam.com>
2016-06-02 19:17:41 +0300

    [TG-87] Added HeaderComponent

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,7 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
 import {RouteConfig, RouterLink, RouterOutlet} from 'angular2/router';
 
-import {Home} from './home';
-import {Dashboard} from './dashboard';
-import {DeviceList} from './components/device-list';
-import {ScenarioListComponent, EditScenarioComponent, CreateScenarioComponent} from './scenarios/';
-import {ListComponent} from './list';
-import {SensorDetail} from './sensor-detail';
+import {HeaderComponent} from './components/header/header.component';
 
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import ShHttpService from './sh-http/sh-http.service.js';
@@ -14,10 +9,11 @@ import ShHttpService from './sh-http/sh-http.service.js';
 import exampleRest from './example.rest.js';
 import style from './app.scss';
 import template from './app.html';
+import routes from './routes';
 
 @Component({
     selector: 'sh-app',
-    directives: [RouterLink, RouterOutlet],
+    directives: [RouterLink, RouterOutlet, HeaderComponent],
     providers: [
         Http,
         HTTP_PROVIDERS,
@@ -26,18 +22,7 @@ import template from './app.html';
     template,
     encapsulation: ViewEncapsulation.None
 })
-@RouteConfig([
-    {path: '/', name: 'Index', component: Home, useAsDefault: true},
-    {path: '/home', name: 'Home', component: Home},
-    {path: '/dashboard', name: 'Dashboard', component: Dashboard},
-    {path: '/device-list', name: 'DeviceList', component: DeviceList},
-    {path: '/list', name: 'List', component: ListComponent},
-    {path: '/scenarios', name: 'ScenarioList', component: ScenarioListComponent},
-    {path: '/scenarios/create', name: 'CreateScenario', component: CreateScenarioComponent},
-    {path: '/scenario/:id', name: 'EditScenario', component: EditScenarioComponent},
-    {path: '/sensors/:id', name: 'SensorDetail', component: SensorDetail},
-    {path: '/sensors/create', name: 'SensorDetail', component: SensorDetail}
-])
+@RouteConfig(routes)
 export class App {
     ngOnInit() {
         console.log('Init App'); // eslint-disable-line
---
+++ src/app/routes.js
@@ -0,0 +1,20 @@
+import {Home} from './home';
+import {Dashboard} from './dashboard';
+import {DeviceList} from './components/device-list';
+import {ScenarioListComponent, EditScenarioComponent, CreateScenarioComponent} from './scenarios/';
+import {ListComponent} from './list';
+import {SensorDetail} from './sensor-detail';
+
+export default [
+    {path: '/', name: 'Index', component: Home, useAsDefault: true, headerName: 'Smart House'},
+    {path: '/home', name: 'Home', component: Home},
+    {path: '/dashboard', name: 'Dashboard', component: Dashboard, headerName: 'Dashboard'},
+    {path: '/device-list', name: 'DeviceList', component: DeviceList, headerName: 'Devices'},
+    {path: '/list', name: 'List', component: ListComponent},
+    {path: '/scenarios', name: 'ScenarioList', component: ScenarioListComponent,
+     headerName: 'Scenarios'},
+    {path: '/scenarios/create', name: 'CreateScenario', component: CreateScenarioComponent},
+    {path: '/scenario/:id', name: 'EditScenario', component: EditScenarioComponent},
+    {path: '/sensors/:id', name: 'SensorDetail', component: SensorDetail},
+    {path: '/sensors/create', name: 'SensorDetail', component: SensorDetail}
+];
