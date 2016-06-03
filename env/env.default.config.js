15776811bb1022b8a26dabf99ee9f1e773c9fa67
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2016-06-04 00:34:34 +0300

    [TG-TECH][fix] restoring BE endpoint for default config

--- env/env.default.config.js
+++ env/env.default.config.js
@@ -2,7 +2,7 @@ const envConfig = require('./env.common.config');
 
 module.exports = Object.assign(envConfig, {
     public: {
-        backEndUrl: '//localhost:8000',
+        backEndUrl: '//localhost:3000',
         backEndWebSocketUrl: '//localhost:3000'
     }
 });
