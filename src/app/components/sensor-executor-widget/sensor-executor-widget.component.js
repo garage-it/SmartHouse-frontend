9edd8fc384a1b6e0739916e1c7af270fea09ef5e
Oleksandr <agoldfarb@ukr.net>
2016-06-16 19:21:06 +0300

    [TG-160] [fix] fixed executor twitching after toggling (#49)
    * [TG-160] [fix] fixed executor twitching after toggling
    
    * [TG-160] [fix] fixed executor twitching when switching state 2x times within short delay
    
    * [TG-160] [fix] increased delay for switcher
    
    * [TG-160] [fix] increased delay for switcher
    
    * [TG-160] [fix] eslint error fix

--- src/app/components/sensor-executor-widget/sensor-executor-widget.component.js
+++ src/app/components/sensor-executor-widget/sensor-executor-widget.component.js
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import {Component} from 'angular2/core';
 
 import BaseSensor from '../shared/base-sensor';
 import template from './sensor-executor-widget.html';
@@ -6,6 +6,9 @@ import styles from '../shared/sensor-widget.scss';
 
 export const DEVICE_ON_STATE = 'ON';
 export const DEVICE_OFF_STATE = 'OFF';
+export const DEVICE_RESPOND_TIMEOUT = 4000;
+const pending = Symbol('pending');
+const timeout = Symbol('timeout');
 
 @Component({
     selector: 'sm-sensor-executor-widget',
@@ -14,12 +17,31 @@ export const DEVICE_OFF_STATE = 'OFF';
     inputs: ['device', 'description']
 })
 export class SensorExecutorWidget extends BaseSensor {
+    constructor(...args) {
+        super(args);
+        this[pending] = null;
+    }
 
     onDeviceDataChanged(data) {
+        // NOTE: mqtt is slow and sends us false information for some time after toggle
+        // we just don`t listen to socket (and mqtt) for some time to let it switch state
+        if (this[pending] !== null) {
+            return;
+        }
+
         this.data.value = data.value === DEVICE_ON_STATE;
     }
 
     switchExecutor($event) {
+        this[pending] = $event.target.checked;
+        this.data.value = $event.target.checked;
+
+        clearTimeout(this[timeout]);
+
+        this[timeout] = setTimeout(() => {
+            this[pending] = null;
+        }, DEVICE_RESPOND_TIMEOUT);
+
         this.sensorWidgetService.pushEvent({
             device: this.device.mqttId,
             value: $event.target.checked ? DEVICE_ON_STATE : DEVICE_OFF_STATE
