851953484b344b5626d931699be9a8be60266ece
Denis Kondakov <denis.n.kondakov@gmail.com>
2016-06-22 11:57:48 +0300

    [TG-166][ref] Changed browser to Chrome

--- env/protractor.conf.js
+++ env/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-        browserName: 'firefox'
+        browserName: 'chrome'
     },
 
     // Spec patterns are relative to the current working directly when
