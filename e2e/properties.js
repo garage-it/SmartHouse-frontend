7b9eb10ed6b0a8a59bf4d1ab5d39060a4c0e8fbe
Denis Kondakov <denis.n.kondakov@gmail.com>
2016-06-22 12:18:18 +0300

    [TG-166][fix] Fixed properties file

--- e2e/properties.js
+++ e2e/properties.js
@@ -1,26 +1,25 @@
 var Properties = function () {
 
-  this.names = {
-    links: {
-      dashboard: "Dashboard",
-      devices: "Devices",
-      scenarios: "Scenarios",
-      backToList: "Back to list"
-    },
-    headers: {
-      dashboard: "Dashboard",
-      devices: "Devices",
-      scenarios: "Scenario list",
-      newDevice: "New device"
-    },
-    title: "Smart Home",
-    logo: "Smart House"
-  },
-  newDevice: {
-    id: "testId",
-    type: "testType",
-    desc: "testDesc"
-  }
-}
-
+    this.names = {
+        links: {
+            dashboard: "Dashboard",
+            devices: "Devices",
+            scenarios: "Scenarios",
+            backToList: "Back to list"
+        },
+        headers: {
+            dashboard: "Dashboard",
+            devices: "Devices",
+            scenarios: "Scenario list",
+            newDevice: "New device"
+        },
+        title: "Smart Home",
+        logo: "Smart House",
+        newDevice: {
+            id: "testId",
+            type: "testType",
+            desc: "testDesc"
+        }
+    };
+};
 module.exports = Properties;
