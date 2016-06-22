572a0b8e0f6586ac71c8be06b6608c9ad0b0fc1c
Denis Kondakov <denis.n.kondakov@gmail.com>
2016-06-22 15:00:58 +0300

    [TG-166][fix] Fixed adding of device. Added WaitUtils module

--- e2e/actions/devices.page.actions.js
+++ e2e/actions/devices.page.actions.js
@@ -1,20 +1,27 @@
-var DevicesPage = require('../pobjects/pages/devices.page.js');
-var AddNewDevicePage = require('../pobjects/pages/add.new.device.page.js');
+var WaitUtils = require('../utils/wait.utils');
+var DevicesPage = require('../pobjects/pages/devices.page');
+var AddNewDevicePage = require('../pobjects/pages/add.new.device.page');
 
 var DevicesPageActions = function () {
     var devicesPage = new DevicesPage();
     var addNewDevicePage = new AddNewDevicePage();
+    var waitUtils = new WaitUtils();
 
     this.addNewDevice = (name, type, description, switcher) => {
         devicesPage.addNewDeviceButton.click().then(() => {
+            waitUtils.waitFor(addNewDevicePage.deviceNameInput);
             addNewDevicePage.deviceNameInput.sendKeys(name);
+            waitUtils.waitFor(addNewDevicePage.deviceTypeInput);
             addNewDevicePage.deviceTypeInput.sendKeys(type);
+            waitUtils.waitFor(addNewDevicePage.deviceDescriptionInput);
             addNewDevicePage.deviceDescriptionInput.sendKeys(description);
-            if (switcher) {
+            if (switcher != null && switcher) {
               addNewDevicePage.switcherLabel.click();
             }
             addNewDevicePage.saveDeviceButton.click();
-        })
+        });
+        waitUtils.waitFor(devicesPage.pageHeader);
     };
 };
+
 module.exports = DevicesPageActions;
--- e2e/tests/add.remove.device.spec.js
+++ e2e/tests/add.remove.device.spec.js
@@ -1,18 +1,18 @@
-var AddNewDevicePage = require('../pobjects/pages/add.new.device.page.js');
 var DevicesPage = require('../pobjects/pages/devices.page.js');
+var DevicesPageActions = require('../actions/devices.page.actions.js');
 var Properties = require('../properties.js');
 
 describe('Add and Remove device Test', () => {
-    var addNewDevicePage = new AddNewDevicePage();
     var devicesPage = new DevicesPage();
+    var devicesPageActions = new DevicesPageActions();
     var props = new Properties();
 
     beforeEach(() => {
-        addNewDevicePage.get();
+        devicesPage.get();
     });
 
     it('should check adding of device', () => {
-        devicesPage.addNewDevice(props.names.newDevice.testId, props.names.newDevice.testType, props.names.newDevice.testDesc);
+        devicesPageActions.addNewDevice(props.names.newDevice.id, props.names.newDevice.type, props.names.newDevice.desc, false);
         expect(devicesPage.pageHeader.getText()).toEqual(props.names.headers.devices);
     });
-  };
+  });
