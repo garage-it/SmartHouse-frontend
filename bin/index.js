badaa3c606e3c47f7331ba4340bc25aebee657a1
Yurii Sorokin <Yurii_Sorokin@epam.com>
2016-06-25 16:50:09 +0300

    "Revert" cd27461 [TG-TECH] [chore] specified cwd for bin

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
 
-const path = require('path');
 const exec = require('shelljs').exec;
-const cwd = path.join(__dirname, '..');
 
-exec('npm run client', {cwd});
+exec('npm run client');
