d6641c8663bba2df4979e3bd63adc65f021ee3b7
Viktor Novak <valakar@gmail.com>
2016-11-08 12:06:02 +0200

    [TG-196] [feat] Confirmation dialogues on deleting items (#127)
    * [TG-196] Implement confirmation dialog component.
    
    * [TG-196] Apply confirmation dialog for deleting devices or scenarios.
    
    * [TG-196] fix tslint validation on CI
    
    * [TG-196] [feat] rename confirm dialog buttons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { ShHttpService } from './sh-http/sh-http.service';
+import { DialogConfirmComponent } from './dialog/dialog-confirm.component';
+import { DialogService } from './dialog/dialog.service';
 
 @NgModule({
     id: module.id,
-    imports: [ CommonModule, HttpModule ],
-    providers: [ ShHttpService ],
-    exports: [ CommonModule ]
+    imports: [
+        CommonModule,
+        HttpModule,
+        MaterialModule.forRoot()
+    ],
+    declarations: [ DialogConfirmComponent ],
+    providers: [ ShHttpService, DialogService ],
+    exports: [ CommonModule ],
+    entryComponents: [ DialogConfirmComponent ]
 })
 export class SharedModule {}
