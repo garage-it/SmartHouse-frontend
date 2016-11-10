bdfdddd05e1852e35bb01e2ea4c6813fa2343d12
Igor Iakymenko <yakimko@gmail.com>
2016-11-10 17:45:01 +0200

    [TG-247][story] Implemented registration (#139)
    * [TG-247][story] Implemented registration
    
    * [TG-247][story] fixed unit test
    
    * [TG-247][story] fixed lint warning
    
    * [TG-247][story] code review comments

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+import { IUserRegistrationData } from '../core/auth/auth.interfaces';
+import { AuthService } from '../core/auth/auth.service';
+
+const style = require('./registration.style.scss');
+const template = require('./registration.template.html');
+
+@Component({
+    template,
+    styles: [style],
+    selector: 'sm-registration'
+})
+export class RegistrationComponent {
+    private registrationSubscription: Subscription;
+    private error: string = '';
+    private user: IUserRegistrationData = {
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    };
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    save() {
+        this.registrationSubscription = this.authService.register(this.user)
+            .subscribe(() => {
+                this.router.navigate(['/']);
+            }, err => {
+                this.error = err.json().error || 'Unable to register';
+            });
+    }
+
+    passwordsMatch() {
+        return this.user.password === this.user.passwordConfirm;
+    }
+
+    ngOnDestroy() {
+        if (this.registrationSubscription) {
+            this.registrationSubscription.unsubscribe();
+        }
+    }
+}
