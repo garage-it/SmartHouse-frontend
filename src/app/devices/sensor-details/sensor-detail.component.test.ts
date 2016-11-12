b727d250e6b9e5f4342fef0a7e4450448e43b98d
Viktor Novak <valakar@gmail.com>
2016-11-12 08:23:28 +0200

    [TG-196] [feat] add confirm dialog on details pages

--- src/app/devices/sensor-details/sensor-detail.component.test.ts
+++ src/app/devices/sensor-details/sensor-detail.component.test.ts
@@ -1,155 +1,164 @@
-import {async, TestBed} from '@angular/core/testing';
-
-import { SensorDetailService } from '../shared/sensor-detail.service';
 import { SensorDetailComponent } from './sensor-detail.component';
 import { Sensor } from './sensor';
 
-import { ActivatedRoute, Router } from '@angular/router';
 import ROUTING from './../../config.routing';
 
-const observableSubscribe = {
-    subscribe(fn) { fn(); }
-};
-
-class SensorDetailComponentServiceMock {
-    get() { return observableSubscribe; }
-    save() { return observableSubscribe; }
-    update() { return observableSubscribe; }
-    delete() { return observableSubscribe; }
-}
-
-class ActivatedRouteMock {
-    private snapshot;
-    constructor(id?) {
-        this.snapshot = {
-            params: {id}
-        };
-    }
-}
-
-class RouterMock {
-    navigate() {}
-}
+import { Observable } from 'rxjs';
 
 describe('sensor-detail', () => {
     let sut;
-    let sensorDetailService;
-    let route;
-    let router;
+    let SensorDetailService;
+    let DialogService;
+    let ActivatedRoute;
+    let Router;
+    let ViewContainerRef;
     const idMock = 'mock';
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [ SensorDetailComponent ],
-            providers: [
-                { provide: SensorDetailService, useClass: SensorDetailComponentServiceMock },
-                { provide: ActivatedRoute, useValue: new ActivatedRouteMock(idMock) },
-                { provide: Router, useClass: RouterMock }
-            ]
-        })
-        .overrideComponent(SensorDetailComponent, {
-            set: {template: 'mocked template'}
-        })
-        .compileComponents()
-        .then(() => {
-            sut = TestBed.createComponent(SensorDetailComponent).componentInstance;
+    beforeEach(() => {
+        DialogService = {
+            confirm: jasmine.createSpy('confirm')
+        };
+        ViewContainerRef = {};
 
-            sensorDetailService = TestBed.get(SensorDetailService);
-            route = TestBed.get(ActivatedRoute);
-            router = TestBed.get(Router);
-
-            spyOn(sensorDetailService, 'get').and.callThrough();
-            spyOn(sensorDetailService, 'save').and.callThrough();
-            spyOn(sensorDetailService, 'update').and.callThrough();
-            spyOn(sensorDetailService, 'delete').and.callThrough();
-            spyOn(observableSubscribe, 'subscribe').and.callThrough();
-            spyOn(router, 'navigate').and.callThrough();
-        });
-    }));
+        ActivatedRoute  = {
+            snapshot: {
+                params: {
+                    id: idMock
+                }
+            }
+        };
+        Router = {
+            navigate: jasmine.createSpy('navigate')
+        };
+        SensorDetailService = {
+            get: jasmine.createSpy('get'),
+            save: jasmine.createSpy('save'),
+            update: jasmine.createSpy('update'),
+            delete: jasmine.createSpy('delete')
+        };
+        sut = new SensorDetailComponent(SensorDetailService, Router, ActivatedRoute, DialogService, ViewContainerRef);
+    });
 
     it('should create new sensor if id isn\'t passed to class', () => {
         expect(sut.sensor).toBeDefined();
     });
 
-    it('should not make get request when creating new sensor', () => {
-        route = new ActivatedRouteMock(ROUTING.CREATE);
-        sut = new SensorDetailComponent(sensorDetailService, router, route);
-        sut.ngOnInit();
-        expect(sensorDetailService.get).not.toHaveBeenCalled();
-    });
-
-    it('should get sensor by given id', () => {
-        sut.ngOnInit();
-        expect(sensorDetailService.get).toHaveBeenCalledWith(idMock);
-    });
-
-    it('should save new sensor', () => {
-        const sensorMock = {};
-        sut.sensor = sensorMock;
-        sut.needUpdate = false;
-        sut.save();
-        expect(sensorDetailService.save).toHaveBeenCalledWith(sensorMock);
-    });
-
-    it('should navigate to the list of sensors on save', (done) => {
-        const sensorMock = {};
-        sut.sensor = sensorMock;
-        sut.needUpdate = true;
-        sut.save();
-        expect(router.navigate).toHaveBeenCalledWith(['/devices']);
-        done();
-    });
-
-    it('should update sensor', () => {
-        const sensorMock = {};
-        sut.sensor = sensorMock;
-        sut.needUpdate = true;
-        sut.save();
-        expect(sensorDetailService.update).toHaveBeenCalledWith(sensorMock);
-    });
-
-    it('should remove sensor', () => {
-        const sensorMock = {};
-        sut.sensor = sensorMock;
-        sut.remove();
-        expect(sensorDetailService.delete).toHaveBeenCalledWith(sensorMock);
-    });
-
-    it('should navigate to the list of sensors on remove', (done) => {
-        const sensorMock = {};
-        sut.sensor = sensorMock;
-        sut.remove();
-        (observableSubscribe.subscribe as jasmine.Spy).calls.mostRecent().args[0]();
-        expect(router.navigate).toHaveBeenCalledWith(['/devices']);
-        done();
-    });
-
-    it('should navigate to the list of devices on cancel', () => {
-        sut.cancel();
-        expect(router.navigate).toHaveBeenCalledWith(['/devices']);
-    });
-
-    it('should not allow both executor and servo to be checked at one time (servo changed)', () => {
-        const sensorMock = new Sensor({
-            servo: true,
-            executor: true
+    describe('#ngOnInit', () => {
+        it('should not make get request when creating new sensor', () => {
+            ActivatedRoute.snapshot.params.id = ROUTING.CREATE;
+            sut = new SensorDetailComponent(SensorDetailService, Router, ActivatedRoute, DialogService, ViewContainerRef);
+            sut.ngOnInit();
+            expect(SensorDetailService.get).not.toHaveBeenCalled();
         });
 
-        sut.sensor = sensorMock;
-        sut.needUpdate = true;
-        sut.onServoChanged();
-        expect(sut.sensor.executor).toBe(false);
+        it('should get sensor by given id', () => {
+            SensorDetailService.get.and.returnValue(Observable.of(idMock));
+            sut.ngOnInit();
+            expect(SensorDetailService.get).toHaveBeenCalledWith(idMock);
+        });
     });
 
-    it('should not allow both executor and servo to be checked at one time (executor changed)', () => {
-        const sensorMock = new Sensor({
-            servo: true,
-            executor: true
+    describe('#save', () => {
+        const sensorMock = {};
+        beforeEach(() => {
+            SensorDetailService.save.and.returnValue(Observable.of(sensorMock));
+            SensorDetailService.update.and.returnValue(Observable.of(sensorMock));
+
+            sut.sensor = sensorMock;
         });
 
-        sut.sensor = sensorMock;
-        sut.needUpdate = true;
-        sut.onExecutorChanged();
-        expect(sut.sensor.servo).toBe(false);
+        it('should save new sensor', () => {
+            sut.needUpdate = false;
+            sut.save();
+            expect(SensorDetailService.save).toHaveBeenCalledWith(sensorMock);
+        });
+
+        it('should navigate to the list of sensors on save', (done) => {
+            sut.needUpdate = true;
+            sut.save();
+            expect(Router.navigate).toHaveBeenCalledWith(['/devices']);
+            done();
+        });
+
+        it('should update sensor', () => {
+            sut.needUpdate = true;
+            sut.save();
+            expect(SensorDetailService.update).toHaveBeenCalledWith(sensorMock);
+        });
+    });
+
+    describe('#cancel', () => {
+        it('should navigate to the list of devices on cancel', () => {
+            sut.cancel();
+            expect(Router.navigate).toHaveBeenCalledWith(['/devices']);
+        });
+    });
+
+    describe('#remove', () => {
+        const sensorMock = {};
+
+        beforeEach(() => {
+            sut.sensor = sensorMock;
+        });
+
+        describe('before user confirmation', () => {
+            beforeEach(() => {
+                DialogService.confirm.and.returnValue(Observable.of(false));
+                sut.remove();
+            });
+
+            it('should show confirm dialog', () => {
+                expect(DialogService.confirm).toHaveBeenCalled();
+            });
+
+            it('should not remove sensor if user cancel confirmation', () => {
+                expect(SensorDetailService.delete).not.toHaveBeenCalled;
+            });
+        });
+
+        describe('should remove sensor if user confirms', () => {
+            beforeEach(() => {
+                SensorDetailService.delete.and.returnValues(Observable.of(1));
+                DialogService.confirm.and.returnValue(Observable.of(true));
+                sut.remove();
+            });
+
+            it('should remove sensor', () => {
+                expect(SensorDetailService.delete).toHaveBeenCalledWith(sensorMock);
+            });
+
+            it('should navigate to the list of sensors on remove', () => {
+                expect(Router.navigate).toHaveBeenCalledWith(['/devices']);
+            });
+        });
+
+    });
+
+    describe('#onExecutorChanged', () => {
+        it('should not allow both executor and servo to be checked at one time (executor changed)', () => {
+            const sensorMock = new Sensor({
+                servo: true,
+                executor: true
+            });
+
+            sut.sensor = sensorMock;
+            sut.needUpdate = true;
+            sut.onExecutorChanged();
+            expect(sut.sensor.servo).toBe(false);
+        });
+    });
+
+    describe('#onServoChanged', () => {
+        it('should not allow both executor and servo to be checked at one time (servo changed)', () => {
+            const sensorMock = new Sensor({
+                servo: true,
+                executor: true
+            });
+
+            sut.sensor = sensorMock;
+            sut.needUpdate = true;
+            sut.onServoChanged();
+            expect(sut.sensor.executor).toBe(false);
+        });
     });
 });
