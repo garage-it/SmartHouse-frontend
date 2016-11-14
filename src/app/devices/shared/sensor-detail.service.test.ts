58456f389bae4636d4fe568a1b60530b0438b685
Viktor Novak <valakar@gmail.com>
2016-11-14 15:09:24 +0200

    [TG-297] [refactor] remove TestBed from devices module (#148)

--- src/app/devices/shared/sensor-detail.service.test.ts
+++ src/app/devices/shared/sensor-detail.service.test.ts
@@ -1,64 +1,43 @@
-import { async, TestBed } from '@angular/core/testing';
-
 import { SensorDetailService } from './sensor-detail.service';
-import { ShHttpService } from '../../core/sh-http/sh-http.service';
-
-const observableMock = { map() {} };
-
-class HttpMock {
-    get() { return observableMock; }
-    post() { return observableMock; }
-    put() { return observableMock; }
-    delete() { return observableMock; }
-}
 
 describe('SensorDetailService', () => {
     let sut;
-    let httpMock;
+    let http;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {provide: ShHttpService, useClass: HttpMock },
-                SensorDetailService
-            ]
-        })
-        .compileComponents()
-        .then(() => {
-            sut = TestBed.get(SensorDetailService);
-            httpMock = TestBed.get(ShHttpService);
-            spyOn(httpMock, 'get').and.callThrough();
-            spyOn(httpMock, 'post').and.callThrough();
-            spyOn(httpMock, 'put').and.callThrough();
-            spyOn(httpMock, 'delete').and.callThrough();
-        });
-    }));
+    const idMock = 'mock';
+    const sensorMock = {_id: idMock};
+
+    beforeEach(() => {
+        http = {
+            get: jasmine.createSpy('get'),
+            post: jasmine.createSpy('post'),
+            put: jasmine.createSpy('put'),
+            delete: jasmine.createSpy('delete')
+        };
+        sut = new SensorDetailService(http);
+    });
 
     it('should be defined', () => {
         expect(sut).toBeDefined();
     });
 
     it('should get sensor data from the server', () => {
-        const idMock = 'mock';
-        sut.get(idMock);
-        expect(httpMock.get).toHaveBeenCalledWith(`/sensors/${idMock}`);
+        sut.get(sensorMock._id);
+        expect(http.get).toHaveBeenCalledWith(`/sensors/${idMock}`);
     });
 
     it('should save sensor', () => {
-        const sensorMock = {_id: 'mock'};
         sut.save(sensorMock);
-        expect(httpMock.post).toHaveBeenCalledWith('/sensors', sensorMock);
+        expect(http.post).toHaveBeenCalledWith('/sensors', sensorMock);
     });
 
     it('should update sensor', () => {
-        const sensorMock = {_id: 'mock'};
         sut.update(sensorMock);
-        expect(httpMock.put).toHaveBeenCalledWith(`/sensors/${sensorMock._id}`, sensorMock);
+        expect(http.put).toHaveBeenCalledWith(`/sensors/${sensorMock._id}`, sensorMock);
     });
 
     it('should delete sensor', () => {
-        const sensorMock = {_id: 'mock'};
         sut.delete(sensorMock);
-        expect(httpMock.delete).toHaveBeenCalledWith(`/sensors/${sensorMock._id}`);
+        expect(http.delete).toHaveBeenCalledWith(`/sensors/${sensorMock._id}`);
     });
 });
