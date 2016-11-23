380e4bd158393ea6ddffb9660c240e6fad2a60e3
Sergey Trotsyuk <sergey.trotsyuk@gmail.com>
2016-11-23 18:11:05 +0200

    TG-246 UT for login component and WindowRef service

--- src/app/login/login.component.test.ts
+++ src/app/login/login.component.test.ts
@@ -3,23 +3,25 @@ import { Observable } from 'rxjs/Rx';
 
 describe('Login component', () => {
     let sut;
-    let AuthService;
+    let authService;
     let router;
     let activateRoute;
     let windowRef;
     let window;
 
+    function createComponent() {
+        return new LoginComponent(authService, router, activateRoute, windowRef);
+    }
+
     beforeEach(() => {
-        AuthService = {
+        authService = {
             login: jasmine.createSpy('login')
         };
         router = {
             navigate: jasmine.createSpy('navigate')
         };
         activateRoute = {
-            params: {
-                subscribe: jasmine.createSpy('params subscribe')
-            }
+            params: Observable.of({})
         };
         windowRef = {
             nativeWindow: window
@@ -29,52 +31,106 @@ describe('Login component', () => {
                 href: null
             }
         };
-        sut = new LoginComponent(AuthService, router, activateRoute, windowRef);
+
     });
 
-    describe('login', () => {
-        let credentials;
+    describe('constructor', () => {
+        it('should show FB login error for URL with parameter', () => {
+            // Spies & stubs
+            activateRoute.params = Observable.of({
+                error: 'fb-error'
+            });
 
+            // Run
+            sut = createComponent();
+
+            // Expect
+            expect(sut.fbLoggingError).toBe(true)
+        });
+
+        describe('should not show FB login error', () => {
+            it('for URL without parameter', () => {
+                // Spies & stubs
+                activateRoute.params = Observable.of({});
+
+                // Run
+                sut = createComponent();
+
+                // Expect
+                expect(sut.fbLoggingError).toBe(false);
+            });
+
+            it('for URL with another parameter value', () => {
+                // Spies & stubs
+                activateRoute.params = Observable.of({
+                    error: 'xxx-error'
+                });
+
+                // Run
+                sut = createComponent();
+
+                // Expect
+                expect(sut.fbLoggingError).toBe(false);
+            });
+        });
+    });
+
+    describe('methods', () => {
         beforeEach(() => {
-            credentials = {
-                email: 'valid@valid.com',
-                password: 'valid-pass'
-            };
-            AuthService.login.and.returnValue(Observable.of(1));
-            sut.credentials = credentials;
-            sut.login();
+            sut = createComponent();
         });
 
-        it('should make atempt to login user with provided credentials', () => {
-            expect(AuthService.login).toHaveBeenCalledWith(credentials);
-        });
+        describe('login', () => {
+            let credentials;
 
-        it('should navigate to home page on success', () => {
-            expect(router.navigate).toHaveBeenCalledWith(['/']);
-        });
-
-        describe('on fail login', () => {
             beforeEach(() => {
-                AuthService.login.and.returnValue(Observable.throw('error'));
+                credentials = {
+                    email: 'valid@valid.com',
+                    password: 'valid-pass'
+                };
+                authService.login.and.returnValue(Observable.of(1));
+                sut.credentials = credentials;
                 sut.login();
             });
 
-            it('should notify about error', () => {
-                expect(sut.loggingError).toBeTruthy();
+            it('should make atempt to login user with provided credentials', () => {
+                expect(authService.login).toHaveBeenCalledWith(credentials);
+            });
+
+            it('should navigate to home page on success', () => {
+                expect(router.navigate).toHaveBeenCalledWith(['/']);
+            });
+
+            describe('on fail login', () => {
+                beforeEach(() => {
+                    authService.login.and.returnValue(Observable.throw('error'));
+                    sut.login();
+                });
+
+                it('should notify about error', () => {
+                    expect(sut.loggingError).toBeTruthy();
+                });
             });
         });
-    });
 
-    describe('on component destroy', () => {
-        beforeEach(() => {
-            sut.loginSubscription = {
-                unsubscribe: jasmine.createSpy('login unsubscribe')
-            };
-            sut.ngOnDestroy();
+        describe('FB login', () => {
+            it('should redirect to FB auth page', () => {
+                sut.loginFb();
+                expect(windowRef.nativeWindow.location.href).toBe(sut.fbLoginUrl);
+            });
         });
 
-        it('should unsubscribe from login events', () => {
-            expect(sut.loginSubscription.unsubscribe).toHaveBeenCalled();
+        describe('on component destroy', () => {
+            beforeEach(() => {
+                sut.loginSubscription = {
+                    unsubscribe: jasmine.createSpy('login unsubscribe')
+                };
+                sut.ngOnDestroy();
+            });
+
+            it('should unsubscribe from login events', () => {
+                expect(sut.loginSubscription.unsubscribe).toHaveBeenCalled();
+            });
         });
     });
 });
