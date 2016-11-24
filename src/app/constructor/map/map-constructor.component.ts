6e06d7b5c5d44f488735874def9e4faaecde6125
oleksandr_hliebov <hliebov@gmail.com>
2016-11-24 22:58:08 +0200

    feat(TG-314): styles

--- src/app/constructor/map/map-constructor.component.ts
+++ src/app/constructor/map/map-constructor.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sh-map-constructor',
-    templateUrl: './map-constructor.template.html'
+    templateUrl: './map-constructor.template.html',
+    styleUrls: [ './map-constructor.style.scss' ]
 })
 export class MapConstructorComponent {}
