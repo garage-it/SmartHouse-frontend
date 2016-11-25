5808bb8a264f31fe67aa87c2dbd6e4d28387cd8b
Appius <appius@outlook.com>
2016-11-25 12:58:50 +0200

    [TG-288][feature] Picture preview with workaround

--- src/app/constructor/constructor.module.ts
+++ src/app/constructor/constructor.module.ts
@@ -4,13 +4,16 @@ import { routing } from './constructor.routing';
 import { ConstructorComponent } from './constructor.component';
 import { MapConstructorComponent } from './map/map-constructor.component';
 import { CommonModule } from '@angular/common';
+import { FileDropDirective, FileSelectDirective } from 'ng2-file-upload';
 
 @NgModule({
     id: module.id,
     imports: [ CommonModule, routing ],
     declarations: [
         ConstructorComponent,
-        MapConstructorComponent
+        MapConstructorComponent,
+        FileDropDirective,
+        FileSelectDirective
     ]
 })
 export class ConstructorModule {}
--- src/app/constructor/map/map-constructor.component.ts
+++ src/app/constructor/map/map-constructor.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+// import { FileItem } from './../../../../node_modules/ng2-file-upload/file-upload/file-item.class';
 
 @Component({
     selector: 'sh-map-constructor',
     templateUrl: './map-constructor.template.html',
     styleUrls: [ './map-constructor.style.scss' ]
 })
-export class MapConstructorComponent {}
+export class MapConstructorComponent {
+    public uploader: FileUploader = new FileUploader({ queueLimit: 1 });
+    public hasBaseDropZoneOver: boolean = false;
+    public picture: any;
+    private reader: FileReader = new FileReader();
+
+    constructor(private ngZone: NgZone) {
+        let self = this;
+
+        this.uploader.onAfterAddingFile = this.ngZone.run(() => function (fileItem: any) { // TODO: must be [fileItem: FileItem]
+            self.reader.readAsDataURL(fileItem._file);
+            this.removeFromQueue(fileItem);
+        });
+
+        this.reader.onload = this.ngZone.run(() => function (event: any) {
+            self.picture = event.target.result;
+        });
+    }
+
+    public fileOverBase(e: any): void {
+        this.hasBaseDropZoneOver = e;
+    }
+}
