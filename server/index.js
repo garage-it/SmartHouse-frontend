00cd894dcb11120ee4bfcec14d1644ef59b6d777
Sergey Trotsyuk <sergey.trotsyuk@gmail.com>
2016-11-29 12:27:07 +0200

    feat(TG-246): Added history API fallback and proxy for FB auth

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('ts-node/register');
 const bodyParser = require('body-parser');
 const path = require('path');
 const express = require('express');
+const expressHttpProxy = require('express-http-proxy');
 const cors = require('cors');
 const app = express();
 const http = require('http').Server(app); // eslint-disable-line
@@ -23,7 +24,17 @@ io.on('connection', socket => {
     socketConfig(socket);
 });
 
+const backendProxy = expressHttpProxy(
+    envConfig.public.backEndUrl.replace(/^\/\//, ''),
+    {
+        forwardPath: (req) => {
+            return req.originalUrl;
+        }
+    }
+);
+
 app.use('/api', router);
+app.use('/auth/login-facebook', backendProxy);
 
 app.post('/emulate', (req, res) => {
     io.emit(req.body.eventName, JSON.parse(req.body.eventData));
@@ -37,6 +48,13 @@ const hostArgs = process.env.RELEASE
     : [envConfig.mockServer.port, envConfig.mockServer.host];
 
 app.use(express.static(staticDir));
+
+// fallback for work with HTML5 History API
+// should be defined after routes (like /api)
+app.get('/*', (req, res) => {
+    res.sendFile(staticDir + '/index.html');
+});
+
 http.listen(...hostArgs, () => {
     console.log(`server listening on ${hostArgs.toString()}`); // eslint-disable-line
 });
