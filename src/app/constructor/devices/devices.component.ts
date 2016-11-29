5f11b38b89f3eb5d191d9faa2a3f2c8202601d74
oleksandr_hliebov <hliebov@gmail.com>
2016-11-30 01:07:44 +0200

    feat(TG-395): DnD

--- src/app/constructor/devices/devices.component.ts
+++ src/app/constructor/devices/devices.component.ts
@@ -1,5 +1,5 @@
 import {OnInit, Component, ViewChildren, AfterViewInit, ElementRef, Renderer} from '@angular/core';
-import {Device} from 'src/app/devices/device.model';
+import {Device} from '../../devices/device.model';
 import {DragulaService} from 'ng2-dragula/ng2-dragula';
 
 @Component({
@@ -15,13 +15,6 @@ export class DevicesComponent implements AfterViewInit{
     constructor(private element: ElementRef,
                 private renderer: Renderer,
                 private dragulaService: DragulaService) {
-
-        dragulaService.drag.subscribe((value) => {
-            console.log(value);
-        });
-        dragulaService.drop.subscribe((value) => {
-            console.log(value);
-        });
     }
 
     ngAfterViewInit() {
@@ -31,21 +24,27 @@ export class DevicesComponent implements AfterViewInit{
                 this.deviceList = this.devices
                     .map(ref => ref.nativeElement);
                 this.drawSensor();
-            })
+            });
+
+        this.dragAndDrop();
     }
 
     drawSensor(): void {
         let len = this.deviceList.length;
         let last = this.deviceList[len - 1];
-        let posX = len * 50;
-        //TODO: change 50 to actual size of the element
-
-        let posY = 0;
-        this.renderer.setElementAttribute(last, 'style', `left: ${posX}px; top: ${posY}px;`);
-
         let sensor = this.sensors[this.sensors.length-1];
-        sensor.posX = posX;
-        sensor.posY = posY;
+        let posX = len * 50;
+        let posY = 0;
+
+        if(!sensor.posX) {
+            sensor.posX = posX;
+        }
+        if (!sensor.posY) {
+            sensor.posY = posY;
+        }
+
+        this.renderer.setElementAttribute(last, 'style', `left: ${sensor.posX}px; top: ${sensor.posY}px;`);
+
         this.renderer.setElementAttribute(last, 'id', sensor._id);
 
     }
@@ -59,5 +58,43 @@ export class DevicesComponent implements AfterViewInit{
         if(!this.sensorIsUnique(sensor)) {
             this.sensors.push(sensor);
         }
+    }
+
+    saveCoordinates(target, x: number, y: number): void {
+        this.sensors.filter(s => s._id == target.id)
+            .forEach((s) => {
+                s.posX = x;
+                s.posY = y;
+            });
+    }
+
+    dragAndDrop(): void {
+        this.dragulaService.drag.subscribe((value) => {
+            let X, Y;
+            let target = value.slice(1)[0];
+            let targetWidth = parseInt(getComputedStyle(target).width);
+            let targetHeight = parseInt(getComputedStyle(target).height);
+            let parent = this.element.nativeElement;
+            let parentSize = parent.getBoundingClientRect();
+            let parentWidth = parseInt(getComputedStyle(parent).width);
+            let parentHeight = parseInt(getComputedStyle(parent).height);
+
+            document.onmouseup = () => {
+                let left = X - parseInt(parentSize.left) -  targetWidth/2;
+                let top = Y - parseInt(parentSize.top) - targetHeight/2;
+                if (left > 0 && left < parentWidth
+                    && top > 0 && top < parentHeight) {
+                    this.renderer.setElementAttribute(target, 'style', `left: ${left}px; top: ${top}px;`);
+                    this.saveCoordinates(target, left, top);
+                }
+                document.onmouseup =  null;
+                document.onmousemove =  null;
+            };
+
+            document.onmousemove = (e)=> {
+                X = e.clientX;
+                Y = e.clientY;
+            }
+        });
     }
 }
