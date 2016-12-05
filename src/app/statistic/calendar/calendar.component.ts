324f05c409688ec51fd12b5481ad9531b5d2cb98
Sergii <Sergii_Kudriashov@epam.com>
2016-12-05 21:47:57 +0200

    (TG-383): Dummy calendar added

---
+++ src/app/statistic/calendar/calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sh-calendar',
+    templateUrl: 'calendar.template.html',
+    styleUrls: ['./calendar.styles.scss']
+})
+export class CalendarComponent {
+    constructor() {
+    }
+
+}
--- src/app/statistic/statistic.declarations.ts
+++ src/app/statistic/statistic.declarations.ts
@@ -2,9 +2,11 @@ import { StatisticComponent } from './statistic.component';
 import { ChartWidgetComponent } from '../statistic/chart-widget/chart-widget.component';
 import { SensorStatisticComponent } from './sensor-statistic/sensor-statistic.component';
 import { SwitcherColumnChartComponent } from './switcher-statistics/switcher-column-chart/switcher-column-chart.component';
+import { CalendarComponent } from './calendar/calendar.component';
 export default [
     StatisticComponent,
     ChartWidgetComponent,
     SensorStatisticComponent,
-    SwitcherColumnChartComponent
+    SwitcherColumnChartComponent,
+    CalendarComponent
 ];
