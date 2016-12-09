7cdca6a70f6c11d723905d62cd1ec35fcde46d14
Viktor Novak <valakar@gmail.com>
2016-12-09 14:32:55 +0200

    feat(TG-425): fix lint issues

--- src/app/constructor/constructor.routing.ts
+++ src/app/constructor/constructor.routing.ts
@@ -2,7 +2,6 @@ import { RouterModule } from '@angular/router';
 
 import { ConstructorComponent } from './constructor.component';
 import { DeviceListResolveService } from '../settings/devices/device-list/device-list-resolve.service';
-import { DashboardViewResolveService } from './dashboard/dashboard-view/dashboard-view-resolve.service';
 
 export const routes = [
     {
