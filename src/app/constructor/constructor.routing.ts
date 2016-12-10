a52178525bb7d49be2982272eef659411e422728
Viktor Novak <valakar@gmail.com>
2016-12-10 15:58:57 +0200

    feat(TG-425): remove dashboard devices from constructor create (#194)
    * feat(TG-425): remove dashboard devices from constructor create
    
    * feat(TG-425): fix lint issues
    
    * feat(TG-425): fix lint issues

--- src/app/constructor/constructor.routing.ts
+++ src/app/constructor/constructor.routing.ts
@@ -2,14 +2,12 @@ import { RouterModule } from '@angular/router';
 
 import { ConstructorComponent } from './constructor.component';
 import { DeviceListResolveService } from '../settings/devices/device-list/device-list-resolve.service';
-import { DashboardViewResolveService } from './dashboard/dashboard-view/dashboard-view-resolve.service';
 
 export const routes = [
     {
         path: '',
         component: ConstructorComponent,
         resolve: {
-            dashboard: DashboardViewResolveService,
             sensors: DeviceListResolveService
         }
     }
