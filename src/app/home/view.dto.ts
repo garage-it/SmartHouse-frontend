ff88d859e0ef9b4f5042fc45b9e7f2b4c7d2819f
Igor Mykhaylyuk <igor13luk@gmail.com>
2016-12-13 03:58:32 -0800

    fix lint issues

--- src/app/home/view.dto.ts
+++ src/app/home/view.dto.ts
@@ -2,11 +2,11 @@ import { MapViewInfoDto } from './map-view/map-view.dto';
 import { Device } from '../shared/devices/device.model';
 
 export interface DashboardViewInfoDto {
-    _id: string,
-    devices: Device
+    _id: string;
+    devices: Device;
 }
 
 export interface ViewInfoDto {
-    mapView?: MapViewInfoDto,
-    dashboard?: DashboardViewInfoDto
+    mapView?: MapViewInfoDto;
+    dashboard?: DashboardViewInfoDto;
 }
