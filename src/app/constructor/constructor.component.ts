b11a775771e911a509514302d5a3e8dc36be4869
Viktor Novak <valakar@gmail.com>
2016-12-20 00:49:59 +0200

    feat(TG-433): add check for required fields

--- src/app/constructor/constructor.component.ts
+++ src/app/constructor/constructor.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ViewInfoDto } from '../home/view/view.dto';
 import { ActivatedRoute } from '@angular/router';
 import { ViewService } from '../home/view/view.service';
+import { ToastsManager } from 'ng2-toastr';
 
 @Component({
     selector: 'sh-constructor',
@@ -13,7 +14,11 @@ export class ConstructorComponent {
     public canBeDashboardActive: boolean = true;
     private view: ViewInfoDto;
 
-    constructor(private route: ActivatedRoute, private viewService: ViewService) {}
+    constructor(
+        private route: ActivatedRoute,
+        private viewService: ViewService,
+        private toastr: ToastsManager
+    ) {}
 
     public ngOnInit(): void {
         this.view = this.route.snapshot.data['view'] || {
@@ -34,9 +39,24 @@ export class ConstructorComponent {
     }
 
     public onSaveView(): void {
+        if (!this.isViewCanBeSaved()) {
+            this.toastr.error('Please fill mandatory fields: "Name", "Description" and (("Add Picture") or ("Dashboard"))');
+            return;
+        }
         this.viewService.create(this.view).subscribe(view => {
             // TODO Save image??? and redirect
             console.log('view saved', view);
         });
+    }
+
+    private isViewCanBeSaved(): boolean {
+        debugger;
+        return this.view.name
+            && this.view.description
+            && (
+                this.view['picture']
+                || this.view.dashboardSubview.devices
+                && this.view.dashboardSubview.devices.length > 0
+            );
     }
 }
