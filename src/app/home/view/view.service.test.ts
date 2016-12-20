91fd651c92b21c462afaad3476227c2155720aec
Viktor Novak <valakar@gmail.com>
2016-12-20 15:22:53 +0200

    feat(TG-433): tested view service

--- src/app/home/view/view.service.test.ts
+++ src/app/home/view/view.service.test.ts
@@ -1,3 +1,36 @@
-/**
- * Created by lelou on 19.12.16.
- */
+import { ViewService } from './view.service';
+
+describe('ViewService', () => {
+    const response = {};
+
+    let sut;
+    let http;
+    let result;
+
+    beforeEach(() => {
+
+        http = {
+            post: jasmine.createSpy('put').and.returnValue(response)
+        };
+
+        sut = new ViewService(http);
+    });
+
+    describe('create', () => {
+
+        const viewInfoDto = {};
+
+        beforeEach(() => {
+            result = sut.create(viewInfoDto);
+        });
+
+        it('should make request to create view', () => {
+            expect(http.post).toHaveBeenCalledWith('/views', viewInfoDto);
+        });
+
+        it('should return response', () => {
+            expect(result).toEqual(response);
+        });
+
+    });
+});
