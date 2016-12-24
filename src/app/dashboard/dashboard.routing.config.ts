02f115f7d7f75b71e860420e7dbb7b459c1e404c
Viktor Novak <valakar@gmail.com>
2016-12-24 14:14:56 +0200

    feat(TG-286): Home page displaying in Dashboard view (#224)
    * feat(TG-286): add widgets to dashboard view on home page
    
    * feat(TG-286): chore redundant dashboard module
    
    * feat(TG-286): refactor dashboard module in sensors module
    
    * feat(TG-286): chore unused widget model
    
    * refactor dashboard on home page

--- src/app/dashboard/dashboard.routing.config.ts
+++
@@ -1,6 +0,0 @@
-const DASHBOARD_ROUTING_CONFIG = {
-    BASE: '',
-    EDITOR: 'editor'
-};
-
-export default DASHBOARD_ROUTING_CONFIG;
--- src/app/dashboard/widget.model.ts
+++
@@ -1,8 +0,0 @@
-import { Device } from '../shared/devices/device.model';
-
-export class Widget {
-    _id?: string;
-    device: Device;
-    hidden: boolean;
-    value: string|number|boolean|null;
-}
