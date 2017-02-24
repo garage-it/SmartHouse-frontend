ca36a76ae00de17efced0fb57d81994ac04513c7
Andrey Kolesnik <andrii_kolesnyk@epam.com>
2017-02-24 15:00:31 +0200

    TG-453 Remove statistic for Hour option from UI (#236)
    * TG-453 Remove statistic for one Day option from UI
    
    * TG-453 Remove statistic for one Day option from UI

--- src/app/statistic/sensor-statistic/sensor-statistic.component.ts
+++ src/app/statistic/sensor-statistic/sensor-statistic.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 const CHART_PERIODS: Array<string> = [
-    'hour',
     'day',
     'month',
     'year'
